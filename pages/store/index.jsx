00deecb9f564c01d00bec5a3e00623f0f21bc1fc
Peter Gamali <petrugamali@gmail.com>
2022-09-23 22:57:05 +0300

    Initialized work in store page

--- pages/store/index.jsx
+++ pages/store/index.jsx
@@ -1,6 +1,14 @@
+import styles from '../../styles/Store.module.css'
+
 const Store = () => {
     return (
-        <></>
+        <>
+            <section className={styles.store}>
+                <div className={styles.wrapper}>
+
+                </div>
+            </section>
+        </>
     )
 }
 
