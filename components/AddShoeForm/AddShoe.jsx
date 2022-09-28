752c6f5763d2878ed0463dc269e4023a8aeca250
Peter Gamali <petrugamali@gmail.com>
2022-09-28 16:42:43 +0300

    Added size inputs to shoe form

--- components/AddShoeForm/AddShoe.jsx
+++ components/AddShoeForm/AddShoe.jsx
@@ -6,9 +6,9 @@ export default function AddShoe() {
         colors: [
             {
                 key: 1,
-                colorName: 'a',
+                colorName: '',
                 colorCode: '',
-                availableSizes: [{ sizeNumber: '', sizeStock: '' }]
+                availableSizes: [{ key: 2, sizeNumber: '', sizeStock: '' }]
             }
         ]
     })
@@ -19,17 +19,39 @@ export default function AddShoe() {
         <form>
             {shoe.colors.map((item, index) => {
                 return (
-                    <input key={item.key} type="text" value={item.colorName} onChange={e => setShoe(prevState => {
-                        let colors = [...prevState.colors]
-                        colors[index].colorName = e.target.value
-                        return { ...prevState, colors }
-                    })} />
+                    <React.Fragment key={item.key}>
+                        <input type="text" value={item.colorName} onChange={e => setShoe(prevState => {
+                            let colors = [...prevState.colors]
+                            colors[index].colorName = e.target.value
+                            return { ...prevState, colors }
+                        })} />
+                        <input type="text" value={item.colorCode} onChange={e => setShoe(prevState => {
+                            let colors = [...prevState.colors]
+                            colors[index].colorCode = e.target.value
+                            return { ...prevState, colors }
+                        })} />
+                        {item.availableSizes.map((sizeInfo, sizeIndex) => {
+                            return (
+                                <React.Fragment key={sizeInfo.key}>
+                                    <input type="text" value={item.availableSizes[sizeIndex].sizeNumber} onChange={e => setShoe(prevState => {
+                                        const newColorsArray = [...prevState.colors]
+                                        newColorsArray[index].availableSizes[sizeIndex].sizeNumber = e.target.value
+                                        return { ...prevState, colors: newColorsArray }
+                                    })} />
+                                    <input type="text" value={item.availableSizes[sizeIndex].sizeStock} onChange={e => setShoe(prevState => {
+                                        const newColorsArray = [...prevState.colors]
+                                        newColorsArray[index].availableSizes[sizeIndex].sizeStock = e.target.value
+                                        return { ...prevState, colors: newColorsArray }
+                                    })} />
+                                </React.Fragment>
+                            )
+                        })}
+                    </React.Fragment>
                 )
             })}
             <button onClick={e => setShoe(prevState => {
                 e.preventDefault()
                 const newArray = [...prevState.colors, { key: 2, colorName: '', colorCode: '', availableSizes: [{ sizeNumber: '', sizeStock: '' }] }]
-                console.log(newArray)
                 return { ...prevState, colors: newArray }
             })}>Add new color</button>
         </form >
