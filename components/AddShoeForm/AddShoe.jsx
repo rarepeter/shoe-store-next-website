50d8d020e7d67150a38484bbb53a97076ddcdc47
Peter Gamali <petrugamali@gmail.com>
2022-09-29 08:54:26 +0300

    Added styling to form page

--- components/AddShoeForm/AddShoe.jsx
+++ components/AddShoeForm/AddShoe.jsx
@@ -3,70 +3,73 @@ import styles from './AddShoe.module.css'
 import { v4 as uuidv4 } from 'uuid'
 
 export default function AddShoe() {
-    const [shoe, setShoe] = useState({
-        colors: [
-            {
-                key: uuidv4(),
-                colorName: '',
-                colorCode: '',
-                availableSizes: [{ key: uuidv4(), sizeNumber: '', sizeStock: '' }]
-            }
-        ]
-    })
+	const [shoe, setShoe] = useState({
+		colors: [
+			{
+				key: uuidv4(),
+				colorName: '',
+				colorCode: '',
+				availableSizes: [{ key: uuidv4(), sizeNumber: '', sizeStock: '' }]
+			}
+		]
+	})
 
-    // console.log(shoe)
-    console.log(shoe.colors[0].availableSizes)
-
-    return (
-        <form className={styles.form}>
-            {shoe.colors.map((item, index) => {
-                return (
-                    <React.Fragment key={item.key}>
-                        <label>Color name:</label>
-                        <input type="text" value={item.colorName} onChange={e => setShoe(prevState => {
-                            let colors = [...prevState.colors]
-                            colors[index].colorName = e.target.value
-                            return { ...prevState, colors }
-                        })} />
-                        <label>Color code:</label>
-                        <input type="text" value={item.colorCode} onChange={e => setShoe(prevState => {
-                            let colors = [...prevState.colors]
-                            colors[index].colorCode = e.target.value
-                            return { ...prevState, colors }
-                        })} />
-                        {item.availableSizes.map((sizeInfo, sizeIndex) => {
-                            return (
-                                <div className={styles.sizes} key={sizeInfo.key}>
-                                    <label>Size:</label>
-                                    <input type="text" value={item.availableSizes[sizeIndex].sizeNumber} onChange={e => setShoe(prevState => {
-                                        const newColorsArray = [...prevState.colors]
-                                        newColorsArray[index].availableSizes[sizeIndex].sizeNumber = e.target.value
-                                        return { ...prevState, colors: newColorsArray }
-                                    })} />
-                                    <label>Size stock:</label>
-                                    <input type="text" value={item.availableSizes[sizeIndex].sizeStock} onChange={e => setShoe(prevState => {
-                                        const newColorsArray = [...prevState.colors]
-                                        newColorsArray[index].availableSizes[sizeIndex].sizeStock = e.target.value
-                                        return { ...prevState, colors: newColorsArray }
-                                    })} />
-                                </div>
-                            )
-                        })}
-                        <button onClick={e => setShoe(prevState => {
-                            e.preventDefault()
-                            let oldColorsArray = [...prevState.colors]                           
-                            oldColorsArray.at(index).availableSizes.push({ key: uuidv4(), sizeNumber: '', sizeStock: '' })                          
-                            const updatedColorsArray = oldColorsArray
-                            return { colors: [...updatedColorsArray] }
-                        })}>Add size</button>
-                    </React.Fragment>
-                )
-            })}
-            <button onClick={e => setShoe(prevState => {
-                e.preventDefault()
-                const newArray = [...prevState.colors, { key: uuidv4(), colorName: '', colorCode: '', availableSizes: [{ key: uuidv4(), sizeNumber: '', sizeStock: '' }] }]
-                return { ...prevState, colors: newArray }
-            })}>Add color</button>
-        </form >
-    )
+	return (
+		<form className={styles.form}>
+			{shoe.colors.map((item, index) => {
+				return (
+					<React.Fragment key={item.key}>
+						<h2>Color {index + 1}</h2>
+						<label>Color name:</label>
+						<input type="text" value={item.colorName} onChange={e => setShoe(prevState => {
+							let colors = [...prevState.colors]
+							colors[index].colorName = e.target.value
+							return { ...prevState, colors }
+						})} />
+						<label>Color code:</label>
+						<input type="text" value={item.colorCode} onChange={e => setShoe(prevState => {
+							let colors = [...prevState.colors]
+							colors[index].colorCode = e.target.value
+							return { ...prevState, colors }
+						})} />
+						{item.availableSizes.map((sizeInfo, sizeIndex) => {
+							return (
+								<div className={styles.sizes} key={sizeInfo.key}>
+									<div className={styles['size-box']}>
+										<label>Size:</label>
+										<input type="number" min='0' value={item.availableSizes[sizeIndex].sizeNumber} onChange={e => setShoe(prevState => {
+											const newColorsArray = [...prevState.colors]
+											newColorsArray[index].availableSizes[sizeIndex].sizeNumber = e.target.value
+											return { ...prevState, colors: newColorsArray }
+										})} />
+									</div>
+									<div className={styles['size-box']}>
+										<label>Size stock:</label>
+										<input type="number" min='0' value={item.availableSizes[sizeIndex].sizeStock} onChange={e => setShoe(prevState => {
+											const newColorsArray = [...prevState.colors]
+											newColorsArray[index].availableSizes[sizeIndex].sizeStock = e.target.value
+											return { ...prevState, colors: newColorsArray }
+										})} />
+									</div>
+								</div>
+							)
+						})}
+						<button onClick={e => setShoe(prevState => {
+							e.preventDefault()
+							console.log(oldColorsArray)
+							let oldColorsArray = [...prevState.colors]
+							oldColorsArray.at(index).availableSizes.push({ key: uuidv4(), sizeNumber: '', sizeStock: '' })
+							const updatedColorsArray = oldColorsArray
+							return { colors: [...updatedColorsArray] }
+						})}>Add size</button>
+					</React.Fragment>
+				)
+			})}
+			<button onClick={e => setShoe(prevState => {
+				e.preventDefault()
+				const newArray = [...prevState.colors, { key: uuidv4(), colorName: '', colorCode: '', availableSizes: [{ key: uuidv4(), sizeNumber: '', sizeStock: '' }] }]
+				return { ...prevState, colors: newArray }
+			})}>Add color</button>
+		</form >
+	)
 }
