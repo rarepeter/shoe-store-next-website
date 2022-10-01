65c1a0f86a322bf3a16d28ceae3b1aa7e9e126e4
Peter Gamali <petrugamali@gmail.com>
2022-10-01 23:31:50 +0300

    Added basic file upload middleware for uploading product images to disk storage

---
+++ pages/api/shoes/image-controller.js
@@ -0,0 +1,32 @@
+import connectToDB from "../../../backend/functions/connectToDB"
+import shoeImageHandler from "../../../backend/handlers/shoeHandler"
+import Shoe from "../../../backend/schemas/Shoe.js"
+import { v4 as uuidv4 } from 'uuid'
+import multer from "multer"
+import path from 'path'
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/public/shoe-images')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({
+    storage: storage
+})
+
+const uploadFiles = upload.array('files', 5)
+
+export default shoeImageHandler
+    .use(uploadFiles)
+    .post(async (req, res) => {
+        try {
+            req.files
+            res.status(200).json({ message: 'Images have been uploaded' })
+        } catch (e) {
+            res.status(500).json({ message: 'An error has occured' })
+        }
+    })
